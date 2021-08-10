ed5b0cd2d8b332d43fa04b969f9df8412c81c67e
Matija Osrečki <matija.osrecki@gmail.com>
2021-08-10 22:10:36 +0200

    cryptokitties frontend

--- Begginer/Lession2/zombiefeeding.sol
+++ Begginer/Lession2/zombiefeeding.sol
@@ -22,19 +22,23 @@ contract ZombieFeeding is ZombieFactory {
   address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
   KittyInterface kittyContract = KittyInterface(ckAddress);
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+      // if species is a kitty, replace last 2 numbers of dna with 99 (to identify them)
+      newDna = newDna - newDna % 100 + 99;
+    }
     _createZombie("NoName", newDna);
+
   }
 
-  // define function here
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    feedAndMultiply(_zombieId, kittyDna);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 
 }
